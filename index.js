ada492a872c5f2b8b23ec7b5f1a60045bded7791
shahrulnizamibrahim <shahrulnizam.ibrahim@shopopx.com>
2025-07-10 10:07:57 +0800

    remove unwanted node

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Customer } = require('./dist/nodes/Customer/Customer.node');
 // Import the credentials - TypeScript exports them as default
-const { CustomerApiKey } = require('./dist/credentials/CustomerApiKey.credentials');
+// const { CustomerApiKey } = require('./dist/credentials/CustomerApiKey.credentials');
 const { CustomerOAuth2 } = require('./dist/credentials/CustomerOAuth2.credentials');
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
         Customer
     ],
     credentials: [
-        CustomerApiKey,
+        // CustomerApiKey,
         CustomerOAuth2
     ]
 };
